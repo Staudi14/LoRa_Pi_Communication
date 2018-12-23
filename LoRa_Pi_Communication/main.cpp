c62bbcfc6cd3821f075eb75745a35f30d9483dee
Staudi <patrik.staudenmayer@outlook.de>
2018-12-23 14:59:00 +0100

    Tested continuous receive mode
    Raspberry was able to receive data with an interrupt sub routine

--- LoRa_Pi_Communication/main.cpp
+++ LoRa_Pi_Communication/main.cpp
@@ -3,35 +3,43 @@
 
 using namespace std;
 
+void onReceive(int);
+
 
 int main(void)
 {
 	int packetSize;
 
-	LoRa.setSPIFrequency(1E6);
+	//LoRa.setSPIFrequency(1E6);
 	LoRa.begin();
 
 	cout << "Waiting on Data" << endl;
 
+	// register the receive callback
+	LoRa.onReceive(onReceive);
+
+	// put the radio into receive mode
+	LoRa.receive();
+
 	while (true)
 	{
-		// try to parse packet
-		packetSize = LoRa.parsePacket();
-		if (packetSize) {
-			// received a packet
-			cout << ("Received packet '");
-
-			// read packet
-			while (LoRa.available()) {
-				cout << ((char)LoRa.read());
-			}
-
-			// print RSSI of packet
-			cout << ("' with RSSI ");
-			cout << (LoRa.packetRssi()) << endl;
-		}
+		
 	}
 
 	LoRa.end();
 	return 0;
+}
+
+void onReceive(int packetSize) {
+	// received a packet
+	cout << ("Received packet '");
+
+	// read packet
+	for (int i = 0; i < packetSize; i++) {
+		cout << ((char)LoRa.read());
+	}
+
+	// print RSSI of packet
+	cout << ("' with RSSI ");
+	cout << (LoRa.packetRssi()) << endl;
 }
